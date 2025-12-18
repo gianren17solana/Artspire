11640679d40eb86741cd1c64e3660683896b64e8
Koipix <koi.devt@gmail.com>
2025-12-19 04:22:16 +0800

    font render fix
    Prompted to use google font package since it works well with flutter's font renderer <3

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
@@ -7,17 +8,38 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF21212e),
-      body: Text(
-        "For the love of human creativity",
-        style: TextStyle(
-          fontFamily: 'Poppins',
-          fontSize: 80,
-          fontWeight: FontWeight.w700,
-          letterSpacing: 1,
-          height: 1.2,
-          color: Colors.white,
-        ),
-      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 60, left: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "For the love of",
+                  style: GoogleFonts.poppins(
+                    fontSize: 62,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: -1,
+                    height: 1.2,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  "human creativity",
+                  style: GoogleFonts.poppins(
+                    fontSize: 62,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: -1,
+                    height: 1.2,
+                    color: const Color(0xFF7A88F2),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ) //Body 
     );
   }
 }
